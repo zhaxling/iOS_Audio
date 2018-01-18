652763d2653351c6752515f16166ee2bb0ed3a4f
ZXL <424206567@qq.com>
2018-01-18 21:30:28 +0800

    提交

---
+++ iOS_Audio/iOS_Audio/Classes/录音/AKRecorder.swift
@@ -0,0 +1,13 @@
+//
+//  STRecorder.swift
+//  iOS_Audio
+//
+//  Created by ZXL on 2018/1/17.
+//  Copyright © 2018年 zxl. All rights reserved.
+//
+
+import UIKit
+
+class AKRecorder: NSObject {
+
+}
